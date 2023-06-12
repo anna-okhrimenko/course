e01ab8cb0adc7cde24e99ce36aedd6f842affb8e
anna-okhrimenko <125506103+anna-okhrimenko@users.noreply.github.com>
2023-06-12 19:05:29 +0300

    header mobile

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -1,1 +1,11 @@
-  
+  // modal 
+
+const modal = document.querySelector('.header-mobile_modal');
+const modalBtnOpen = document.querySelector('.toggler');
+const modalBtnClose = document.querySelector('.modal-close');
+modalBtnOpen.addEventListener('click', () => {
+    modal.classList.add('open');
+});
+modalBtnClose.addEventListener('click', () => {
+    modal.classList.remove('open');
+});
