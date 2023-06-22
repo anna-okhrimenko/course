10febe6a55e07caeff580bf69d83446876c48541
anna-okhrimenko <125506103+anna-okhrimenko@users.noreply.github.com>
2023-06-22 22:04:15 +0300

    secret block mobile

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,11 +1,47 @@
-  // modal 
+$(document).ready(function () {
+  $('.slider-secret-block').slick({
+    dots: false,
+    arrows: false,
+    autoplay: false,
+    infinite: true,
+    speed: 300,
+    autoplaySpeed: 4000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 599,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+      
+    ]
+    
+  });
+
+});
+  
+// modal 
 
 const modal = document.querySelector('.header-mobile_modal');
 const modalBtnOpen = document.querySelector('.toggler');
 const modalBtnClose = document.querySelector('.modal-close');
 modalBtnOpen.addEventListener('click', () => {
-    modal.classList.add('open');
+  modal.classList.add('open');
 });
 modalBtnClose.addEventListener('click', () => {
-    modal.classList.remove('open');
+  modal.classList.remove('open');
 });
