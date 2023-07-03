60cc103344ad61f1ab178194bbfedd1e3bf8fed4
anna-okhrimenko <125506103+anna-okhrimenko@users.noreply.github.com>
2023-07-03 21:45:45 +0300

    countdown block desctop

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -32,6 +32,60 @@ $(document).ready(function () {
     
   });
 
+  
+  var targetDate = new Date($( "#countdown" ).attr("data-target"));  
+  var days;
+  var hrs;
+  var min;
+  var sec;
+
+  $(function() {
+    timeToLaunch();
+    setTimeout(countDownTimer,1001);
+  });
+
+  function timeToLaunch(){
+    var currentDate = new Date();
+
+    var diff = (currentDate - targetDate)/1000;
+    var diff = Math.abs(Math.floor(diff));  
+
+    days = Math.floor(diff/(24*60*60));
+    sec = diff - days * 24*60*60;
+
+    hrs = Math.floor(sec/(60*60));
+    sec = sec - hrs * 60*60;
+
+    min = Math.floor(sec/(60));
+    sec = sec - min * 60;
+  }
+
+
+  function countDownTimer(){ 
+      
+    timeToLaunch();
+    
+    $( "#days .number" ).text(days);
+    $( "#hours .number" ).text(hrs);
+    $( "#minutes .number" ).text(min);
+    $( "#seconds .number" ).text(sec);
+    
+    setTimeout(countDownTimer,1000);
+  }
+
+  function numberTransition(id, endPoint, transitionDuration, transitionEase){
+    $({numberCount: $(id).text()}).animate({numberCount: endPoint}, {
+        duration: transitionDuration,
+        easing:transitionEase,
+        step: function() {
+          $(id).text(Math.floor(this.numberCount));
+        },
+        complete: function() {
+          $(id).text(this.numberCount);
+        }
+    }); 
+  };
+
 });
   
 // modal 
